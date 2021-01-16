52806596d4b68f6b76e4bb5012789541a335a185
niole <niolenelson@gmail.com>
2021-01-16 14:27:45 -0800

    Add neg deleteing logic

--- production/destroy.sh
+++ production/destroy.sh
@@ -17,4 +17,27 @@ kubectl delete -Rf ./production/k8s/storage/
 echo -e "\nDestroying secrets"
 kubectl delete -f ./production/k8s/secrets/
 
-# TODO destroy NEGs
+echo -e "\nDestroying NEGs"
+echo -e "\nDestroying deployment-service NEG"
+gcloud compute network-endpoint-groups list | \
+grep deployment-service | \
+awk '{print $1}' | \
+while IFS= read -r line; do; gcloud compute network-endpoint-groups delete $line ; done
+
+echo "Destroying event-service NEG"
+gcloud compute network-endpoint-groups list | \
+grep event-service | \
+awk '{print $1}' | \
+while IFS= read -r line; do; gcloud compute network-endpoint-groups delete $line ; done
+
+echo "Destroying peripheral-service NEG"
+gcloud compute network-endpoint-groups list | \
+grep peripheral-service | \
+awk '{print $1}' | \
+while IFS= read -r line; do; gcloud compute network-endpoint-groups delete $line ; done
+
+echo "Destroying user-service NEG"
+gcloud compute network-endpoint-groups list | \
+grep user-service | \
+awk '{print $1}' | \
+while IFS= read -r line; do; gcloud compute network-endpoint-groups delete $line ; done
